5f38865880ddb97398b91eb728684e8b3508d56e
Trịnh Quang Toàn Hảo <142070040+nguola@users.noreply.github.com>
2024-04-09 22:58:38 +0700

    khung

---
+++ src/connectDB/ConnectDB.java
@@ -0,0 +1,36 @@
+package connectDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDB {
+	public static Connection con = null;
+	private static ConnectDB instance = new ConnectDB();
+	
+	public static ConnectDB getInstance() {
+		return instance;
+	}
+	
+	public void connect() throws SQLException{
+		String url = "jdbc:sqlserver://localhost:1433;databasename=QLCuaHang";
+		String user = "sa";
+		String password = "123";
+		
+		con = DriverManager.getConnection(url, user, password);
+	}
+	
+	public void disconnect() {
+		if(con != null) {
+			try {
+				con.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static Connection getConnection() {
+		return con;
+	}
+}
