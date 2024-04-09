4bc7c9b5e428571240b2c7a5fccca3457ebb29f1
Trịnh Quang Toàn Hảo <142070040+nguola@users.noreply.github.com>
2024-04-10 00:36:20 +0700

    THao

---
+++ src/gui/TrangChu_GUI.java
@@ -0,0 +1,33 @@
+package gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class TrangChu_GUI extends JFrame implements ActionListener{
+	private static final long serialVersionUID = 1L;
+
+	public TrangChu_GUI() {
+		// tạo giao diện chiều rộng 1500 chiều cao 800 và vị trí tương đối giữa màng hình
+		super();
+		setSize(1500, 800);
+		setLocationRelativeTo(null);
+		this.setTitle("Cửa hàng tiện lợi Goods Store");
+		
+		// code
+		
+		//
+		
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new TrangChu_GUI();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+}
