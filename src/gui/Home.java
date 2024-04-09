4d5582cb4584bb299b0a11f51ae2ae8c9c23a106
Trịnh Quang Toàn Hảo <142070040+nguola@users.noreply.github.com>
2024-04-09 23:12:41 +0700

    THao

--- src/gui/Home.java
+++ src/gui/Home.java
@@ -1,5 +1,18 @@
 package gui;
 
-public class Home {
+import javax.swing.JFrame;
 
+public class Home extends JFrame{
+	public Home() {
+		// tạo giao diện chiều rộng 1500 chiều cao 800 và vị trí tương đối giữa màng hình
+		super();
+		setSize(1500, 800);
+		setLocationRelativeTo(null);
+		
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new Home();
+	}
 }
