0e26d139918e26e64d20dc27fcf6a566d00d7b8e
BuiThienHoang221204 <140410385+BuiThienHoang221204@users.noreply.github.com>
2024-04-17 22:18:43 +0700

    Hoang
    update panel_QuanLiKhachHang

--- src/gui/Panel_QuanLiKhachHang.java
+++ src/gui/Panel_QuanLiKhachHang.java
@@ -3,7 +3,11 @@ package gui;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -15,19 +19,30 @@ import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
+import connectDB.ConnectDB;
+import dao.KhachHang_DAO;
 import entity.KhachHang;
+import entity.NhanVien;
 
-public class Panel_QuanLiKhachHang extends JFrame{
+public class Panel_QuanLiKhachHang extends JFrame implements ActionListener{
 	private JTextField txtTimKiem;
 	private TableModel khachhangModel;
 	private JTable tableKhachHang;
+	private KhachHang_DAO khachhang_dao;
 	private ArrayList<KhachHang> listKhachHang = new ArrayList<KhachHang>();
 	
 	
 	public Panel_QuanLiKhachHang() {
+		try {
+			ConnectDB.getInstance().connect();
+			System.out.println("Connected!!!");
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		khachhang_dao = new KhachHang_DAO();
 		setLayout(null);
 		//chừng sau xóa
-		setSize(900,680);
+		setSize(1000,680);
 		setVisible(true);
 		//------------
 		JLabel lblTitle = new JLabel("QUẢN LÝ KHÁCH HÀNG");
@@ -70,12 +85,33 @@ public class Panel_QuanLiKhachHang extends JFrame{
 		khachhangModel = new DefaultTableModel(col,0);
 		tableKhachHang = new JTable(khachhangModel);
 		JScrollPane scroll = new JScrollPane(tableKhachHang);
-		scroll.setBounds(10, 126, 840, 468);
+		scroll.setBounds(10, 126, 950, 468);
 		add(scroll);
+		//đọc dữ liệu vào bảng
+		DocDuLieuDatabaseVaoTable();
 		
+		
+	}
+	public void DocDuLieuDatabaseVaoTable() {
+		List<KhachHang> list = khachhang_dao.getalltbKhachHang();
+		for(KhachHang kh : list) {
+			((DefaultTableModel) khachhangModel).addRow(new Object[] {
+					kh.getMaKhachHang(),
+					kh.getTen(),
+					kh.getSoDienThoai(),
+					kh.getDiaChi(),
+					kh.getLoaiKhachHang()
+			});
+		}
 	}
 	public static void main(String[] args) {
 		new Panel_QuanLiKhachHang();
 	}
-	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 }
