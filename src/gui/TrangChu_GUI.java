c73b082d8bf20f9156141dcc2be87b856149abde
Trịnh Quang Toàn Hảo <142070040+nguola@users.noreply.github.com>
2024-05-06 10:32:42 +0700

    THao

--- src/gui/TrangChu_GUI.java
+++ src/gui/TrangChu_GUI.java
@@ -2,18 +2,14 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 
 import javax.swing.*;
 
 public class TrangChu_GUI extends JFrame implements ActionListener {
-	private static final long serialVersionUID = 1L;
 	private JPanel jp_North;
 	private JLabel lb_Header;
 	private JLabel background;
